2cd84bf7e79bdb60d813e95e66b27d8441920c6a
karani-ken <academice79@gmail.com>
2023-08-26 15:34:29 +0300

    first commit with user model

---
+++ Project-management-system/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_management_system.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-VF9PAJV;Initial Catalog=Project-Management-System;User ID=karani-ken;Password=***********");
+        }
+       // protected override void OnModelCreating(ModelBuilder modelBuilder)
+       // {
+            
+     //   }
+
+    }
+}
---
+++ Project-management-system/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_management_system.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string username { get; set; } = string.Empty;
+        [Required]
+        public string password { get; set; } = string.Empty;
+        [ForeignKey("TaskId")]
+        public int TaskId { get; set; }
+        public Task? Task { get; set; }
+
+
+    }
+}
