c5752d9bb92d4467fc027b71660e84a9b3fb9972
muchoki_esther <muchokiesther8@gmail.com>
2023-08-28 23:30:37 +0300

    Menu bar

--- Project-management-system/Controller/AdminController.cs
+++ Project-management-system/Controller/AdminController.cs
@@ -2,6 +2,9 @@
 using project_management.Data;
 using project_management.Models;
 using project_management.Services;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace project_management.Contoller
 {
@@ -9,79 +12,136 @@ namespace project_management.Contoller
     {
         AdminServices services = new AdminServices();
         ApplicationDbContext _context = new ApplicationDbContext();
-        public async Task CreateProject(string projectname)
+
+        public async Task Init()
         {
+            Console.WriteLine("Admin Menu:");
+            Console.WriteLine("1. Create Project");
+            Console.WriteLine("2. Update Project");
+            Console.WriteLine("3. View All Projects");
+            Console.WriteLine("4. Add Task for Project");
+            Console.WriteLine("5. Delete Project");
+            Console.WriteLine("6. Exit");
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    await CreateProject();
+                    break;
+                case "2":
+                    await UpdateProject();
+                    break;
+                case "3":
+                    await GetAllProjects();
+                    break;
+                case "4":
+                    await CreateNewTask();
+                    break;
+                case "5":
+                    await DeleteProject();
+                    break;
+
+                case "6":
+                    Console.WriteLine("Exiting Admin Menu...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    await Init();
+                    break;
+            }
+
+            await Init();
+        }
+
+        public async Task CreateProject()
+        {
+            Console.WriteLine("Enter Project Name:");
+            var projectName = Console.ReadLine();
+
+            // Create a new project
             var newProject = new Project()
             {
-                ProjectName = projectname,
+                ProjectName = projectName,
             };
+
             await services.AddProjectAsync(newProject);
+            Console.WriteLine("Project created successfully!");
         }
-        //update project
+
         public async Task UpdateProject()
         {
             await GetAllProjects();
-            await Console.Out.WriteLineAsync("Project Id:");
+            Console.WriteLine("Enter Project Id:");
             var pid = Console.ReadLine();
             int projectId = int.Parse(pid);
-            await Console.Out.WriteLineAsync("Project name:");
-            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter New Project Name:");
+            var newName = Console.ReadLine();
+
+            // Create an updated project instance
             var updatedP = new Project()
             {
-                ProjectName = name
-                
+                ProjectName = newName
             };
 
             await services.UpdateProjectAsync(updatedP, projectId);
-
+            Console.WriteLine("Project updated successfully!");
         }
-        //get all projects
+
         public async Task GetAllProjects()
         {
             var projects = await services.GetAllProjectsAsync();
             foreach (var project in projects)
             {
                 Console.WriteLine($"{project.ProjectId}. {project.ProjectName}");
-                foreach(var task in project.projectTasks)
+                foreach (var task in project.projectTasks)
                 {
-                    await Console.Out.WriteLineAsync($"{task.ProjectId} . {task.TaskName}");
+                    Console.WriteLine($"{task.ProjectId}. {task.TaskName}");
                 }
             }
         }
-       //Add a task for a certain project
-       public async Task CreateNewTask()
+
+        public async Task CreateNewTask()
         {
             await GetAllProjects();
-            await Console.Out.WriteLineAsync("Project Id:");
+            Console.WriteLine("Enter Project Id:");
             var pid = Console.ReadLine();
-            int ProjectId = int.Parse(pid);
-            await Console.Out.WriteLineAsync("Task name:");
-            var name = Console.ReadLine();
-            var projectToAddTask =  _context.Projects.FirstOrDefaultAsync(x=> x.ProjectId == ProjectId);
-            if(projectToAddTask != null)
+            int projectId = int.Parse(pid);
+
+            Console.WriteLine("Enter Task Name:");
+            var taskName = Console.ReadLine();
+
+            // Find the project to add the task to
+            var projectToAddTask = _context.Projects.FirstOrDefaultAsync(x => x.ProjectId == projectId);
+
+            if (projectToAddTask != null)
             {
                 var newTask = new ProjectTasks()
                 {
-                    TaskName=name,
-                    ProjectId=ProjectId 
+                    TaskName = taskName,
+                    ProjectId = projectId
                 };
                 await services.AddTaskAsync(newTask);
+                Console.WriteLine("Task added successfully!");
             }
-
-
+            else
+            {
+                Console.WriteLine("Project not found.");
+            }
         }
-        //delete project
 
         public async Task DeleteProject()
         {
             await GetAllProjects();
-            Console.WriteLine("Type in project ID");
+            Console.WriteLine("Enter Project ID:");
             var pid = Console.ReadLine();
-            int ProjectId = int.Parse(pid);
-            await services.DeleteProjectAsync(ProjectId);
+            int projectId = int.Parse(pid);
 
+            await services.DeleteProjectAsync(projectId);
+            Console.WriteLine("Project deleted successfully!");
         }
-        
 
     }
 }
--- Project-management-system/Program.cs
+++ Project-management-system/Program.cs
@@ -1,12 +1,20 @@
 ﻿
+using project_management.Contoller;
 using Project_management_system.Controller;
 
 class Program
 {
-    
-    static async Task Main(string[] args)
-    {
-       
-        await UserController.Initialize();
+
+
+  
+
+       static async Task Main(string[] args)
+        {
+            var adminController = new AdminController();
+            await adminController.Init();
+
+            await UserController.Initialize();
+        }
     }
-}
+
+
